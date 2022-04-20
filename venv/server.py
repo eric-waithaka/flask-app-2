f7233a512717044253574ff38d0656715e752ffd
eric-waithaka <eric.email>
2022-04-20 08:36:54 +0300

    first commit

---
+++ venv/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/index.html')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/components.html')
+def components():
+    return render_template('components.html') 
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
